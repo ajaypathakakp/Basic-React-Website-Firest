9ca2ca2b8438a3708008009387b197c4e8632e14
Ajay Pathak <ajaypathakakp@gmail.com>
2024-02-18 14:02:14 +0530

    Dynamic value call was done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+const description = reactDescriptions[genRandomInt(2)];
+
 import reactImg from './assets/react-core-concepts.png';
 function Header() {
+
   return (
    
    <header>
     <img src={reactImg} alt="Stylized atom" />
     <h1>React Essentials</h1>
     <p>
-      Fundamental React concepts you will need for almost any app you are
+      {description} React concepts you will need for almost any app you are
       going to build!
     </p>
   </header>
